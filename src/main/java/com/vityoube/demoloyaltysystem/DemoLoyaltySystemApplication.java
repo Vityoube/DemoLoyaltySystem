32ac82ca11486b52d2ae9fc2cfaaee1e82a87ac8
vityoube <vityoube@gmail.com>
2023-06-04 16:02:43 +0300

    Initial Commit

---
+++ src/main/java/com/vityoube/demoloyaltysystem/DemoLoyaltySystemApplication.java
@@ -0,0 +1,13 @@
+package com.vityoube.demoloyaltysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLoyaltySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLoyaltySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vityoube/demoloyaltysystem/DemoLoyaltySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vityoube.demoloyaltysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLoyaltySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
